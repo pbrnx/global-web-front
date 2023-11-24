4e09abfe90c3a914f6cf3e1a62f69aad555f2612
pbrnx <pedrobarone2709@gmail.com>
2023-11-24 15:58:50 -0300

    correção no footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Rodape() {
     <div className="footer-container">
       <footer>
         <div className="footer-content">
-          <p>&copy; 2023 - RM99781 e RM551579 — Uma parceria <a href='https://www.fiap.com.br' target='_blank'> FIAP </a> e <a href='https://www.hapvida.com.br/site/' target='_blank'>Hapvida</a></p>
+          <p>&copy; 2023 - RM99781 e RM551579 — Uma parceria <a href='https://www.fiap.com.br' target='_blank'> FIAP </a> e <a href='https://www.hapvida.com.br/site/' target='_blank'>Hapvida</a> — <a href='https://github.com/pbrnx/global-web-front' target='_blank'>Acesse nosso repositório</a></p>
 
           <p>Todos os direitos reservados.</p>
         </div>
