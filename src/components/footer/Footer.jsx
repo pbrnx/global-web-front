03120a81400918045da1fa2ae1dd2384173437e4
Pedro Barone <128100284+pbrnx@users.noreply.github.com>
2024-02-22 19:30:46 -0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Rodape() {
     <div className="footer-container">
       <footer>
         <div className="footer-content">
-          <p>&copy; 2023 - RM99781 e RM551579 — Uma parceria <a href='https://www.fiap.com.br' target='_blank'> FIAP </a> e <a href='https://www.hapvida.com.br/site/' target='_blank'>Hapvida</a> — <a href='https://github.com/pbrnx/global-web-front' target='_blank'>Acesse nosso repositório</a></p>
+          <p>&copy; Webapp feito por: Pedro Augusto Barone — Uma parceria <a href='https://www.fiap.com.br' target='_blank'> FIAP </a> e <a href='https://www.hapvida.com.br/site/' target='_blank'>Hapvida</a> — <a href='https://github.com/pbrnx/global-web-front' target='_blank'>Acesse nosso repositório</a></p>
 
           <p>Todos os direitos reservados.</p>
         </div>
